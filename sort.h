cf26e9f09b05779c128ff9fc1d6896b239d14b5c
Tsehafetizaz <wendetefe@gmail.com>
2024-01-23 09:37:37 -0800

    preey

--- sort.h
+++ sort.h
@@ -27,4 +27,5 @@ void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif /* SORT_H */
